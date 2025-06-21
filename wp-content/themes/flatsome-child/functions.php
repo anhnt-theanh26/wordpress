4873a14938c0402dfc0ecf971f3f44acdda8ccee
anhnt-theanh26 <nguyentheanh260204@gmail.com>
2025-06-21 21:31:40 +0700

    21.06

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -1,2 +1,10 @@
 <?php
 // Add custom Theme Functions here
+
+function load_bootstrap_assets()
+{
+    wp_enqueue_style('css_bootstrap', get_template_directory_uri() . './css/bootstrap.min.css', array(), '5.3.7', 'all');
+    wp_enqueue_script('js_bootstrap', get_template_directory_uri() . './js/bootstrap.min.js', array('jquery'), '5.3.7', true);
+}
+
+add_action('wp_enqueue_scripts', 'load_bootstrap_assets');
