12b9ca3c734c102754d740be6ba60f5c42caaa97
anhnt-theanh26 <nguyentheanh260204@gmail.com>
2025-06-22 21:13:20 +0700

    22/06

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -3,8 +3,8 @@
 
 function load_bootstrap_assets()
 {
-    wp_enqueue_style('css_bootstrap', get_template_directory_uri() . './css/bootstrap.min.css', array(), '5.3.7', 'all');
-    wp_enqueue_script('js_bootstrap', get_template_directory_uri() . './js/bootstrap.min.js', array('jquery'), '5.3.7', true);
+    wp_enqueue_style('css_bootstrap', get_stylesheet_directory_uri() . '/css/bootstrap.min.css', array(), '5.3.7', 'all');
+    wp_enqueue_script('js_bootstrap', get_stylesheet_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '5.3.7', true);
 }
 
 add_action('wp_enqueue_scripts', 'load_bootstrap_assets');
--- wp-content/themes/hocwp/header-v2.php
+++ wp-content/themes/hocwp/header-v2.php
@@ -1,14 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Học lập trình theme Wordpress</title>
+    <title>header version 2 </title>
     <?php wp_head(); ?>
 </head>
-
 <body>
-    <!-- hiển thị thanh navbar menu -->
-    <?php get_template_part('includes/section', 'menu'); ?>
+    
